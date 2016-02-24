09eab89ac9f5e4f87c0675c37840b4b648822dad
Nacho <Nacho@10.0.169.120>
2016-02-24 10:49:25 +0100

    El Buen Tema

---
+++ theme_Mitema/__openerp__.py
@@ -0,0 +1,27 @@
+{
+  'name':'Mitema',
+  'description': 'Best Theme EUWEST ever No scope 360',
+  'version':'1.0',
+  'author':'Nacho',
+  'category': 'Theme/Hidden',
+  'data': [
+        'views/templates.xml',
+    ],
+  'sequence': 1000,
+  'images': [
+        'static/description/cover.png',
+    ],
+  'depends': ['website'],
+  'application': False,
+}
+
+
+    
+    
+    
+    
+    
+  
+    
+    
+    
