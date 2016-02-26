e84aa24d8d69d6f2fef35ee87f067050f538a16a
Nacho <Nacho@10.0.169.120>
2016-02-26 11:41:47 +0100

    para dani el malo

--- theme_Mitema/__openerp__.py
+++ theme_Mitema/__openerp__.py
@@ -3,8 +3,10 @@
   'description': 'Best Theme EUWEST ever No scope 360',
   'version':'1.0',
   'author':'Nacho',
-  'category': 'Theme/Hidden',
-  'data': [ 'views/layout.xml' ],
+  
+  'data': ['views/layout.xml','views/pages.xml','views/assets.xml','views/snippets.xml'],
+  'category': 'Theme/Creative',
+  
   'sequence': 1000,
   'images': [
         'static/description/cover.png',
