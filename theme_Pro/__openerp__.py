15788f01ebac3dbd756609d3339aa6bb554a3a2b
Nacho <Nacho@10.0.169.120>
2016-03-02 12:25:39 +0100

    Casi

---
+++ theme_Pro/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Unicornios rosas',
+    'description': '0.9',
+'category': 'Theme/Creative',
+    'version': '1.0',
+    'author': 'NACHO S.A.',
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml', 
+        'views/assets.xml',
+        'views/snippets.xml'     
+    ],
+    'depends': ['website']
+}
